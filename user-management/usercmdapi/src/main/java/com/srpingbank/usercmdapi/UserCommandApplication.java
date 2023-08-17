f86bffcd28b493db91bf17227e9002aba7b37f51
Cepraga Mihail <yahim09@gmail.com>
2023-08-17 08:46:36 +0300

    implement Axon configuration

--- user-management/usercmdapi/src/main/java/com/srpingbank/usercmdapi/UserCommandApplication.java
+++ user-management/usercmdapi/src/main/java/com/srpingbank/usercmdapi/UserCommandApplication.java
@@ -2,8 +2,12 @@ package com.srpingbank.usercmdapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.springbank.usercore.configuration.AxonConfig;
 
 @SpringBootApplication
+@Import({AxonConfig.class})
 public class UserCommandApplication {
 
 	public static void main(String[] args) {
