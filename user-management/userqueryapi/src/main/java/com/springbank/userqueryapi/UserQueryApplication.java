ea30a778d6fb8724c470932007c38a151efc1d72
Cepraga Mihail <yahim09@gmail.com>
2023-08-17 08:47:49 +0300

    implement Axon configuration

--- user-management/userqueryapi/src/main/java/com/springbank/userqueryapi/UserQueryApplication.java
+++ user-management/userqueryapi/src/main/java/com/springbank/userqueryapi/UserQueryApplication.java
@@ -2,8 +2,12 @@ package com.springbank.userqueryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.springbank.usercore.configuration.AxonConfig;
 
 @SpringBootApplication
+@Import({AxonConfig.class})
 public class UserQueryApplication {
 
 	public static void main(String[] args) {
