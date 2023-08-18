8d023f87f76adaf4f9ae5b7babebfed87f5aa036
Cepraga Mihail <yahim09@gmail.com>
2023-08-18 17:00:51 +0300

    implement Spring Data JPA

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -5,26 +5,45 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner{
 
     // @Autowired
     // private CourseJdbcRepository repository;
+    // @Autowired
+    // private CourseJpaRepository repository;
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course("Microservices!!!", "in28minutes"));
-        repository.insert(new Course("Learn AWS", "in28minutes"));
-        repository.insert(new Course("Learn Azure", "in28minutes"));
-        repository.insert(new Course("Learn DevOps", "in28minutes"));
+        repository.save(new Course("Microservices!!!", "in28minutes"));
+        repository.save(new Course("Learn AWS", "in28minutes"));
+        repository.save(new Course("Learn Azure", "in28minutes"));
+        repository.save(new Course("Learn DevOps", "in28minutes"));
 
         repository.deleteById(2L);
 
         System.out.println(repository.findById(1L));
         System.out.println(repository.findById(3L));
+        System.out.println(repository.findByName("Learn Azure"));
+        System.out.println(repository.count());
     }
+
+
+    // @Override
+    // public void run(String... args) throws Exception {
+    //     repository.insert(new Course("Microservices!!!", "in28minutes"));
+    //     repository.insert(new Course("Learn AWS", "in28minutes"));
+    //     repository.insert(new Course("Learn Azure", "in28minutes"));
+    //     repository.insert(new Course("Learn DevOps", "in28minutes"));
+
+    //     repository.deleteById(2L);
+
+    //     System.out.println(repository.findById(1L));
+    //     System.out.println(repository.findById(3L));
+    // }
     
 }
