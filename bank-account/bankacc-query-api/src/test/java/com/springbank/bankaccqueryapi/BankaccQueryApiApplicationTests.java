216ac185ce2d9a8c9f73113e0a00e66a7a20330a
cepragamihail <118988444+cepragamihail@users.noreply.github.com>
2023-12-30 21:38:11 +0000

    init bank account modules

---
+++ bank-account/bankacc-query-api/src/test/java/com/springbank/bankaccqueryapi/BankaccQueryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbank.bankaccqueryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankaccQueryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
